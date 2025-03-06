05f5abe7f8f17ed3a0b622239ee157057b74331d
AmitBiswas1402 <amit.142biswas@gmail.com>
2025-03-06 21:19:01 +0530

    Add InterviewScheduleUI, LoaderUI, and user role handling to SchedulePage; introduce new UI components including Avatar, Badge, and Calendar

--- src/app/(root)/schedule/page.tsx
+++ src/app/(root)/schedule/page.tsx
@@ -1,6 +1,20 @@
+"use client"
+
+import InterviewScheduleUI from "@/components/InterviewScheduleUI"
+import LoaderUI from "@/components/LoaderUI"
+import { useUserRole } from "@/hooks/useUserRole"
+import { useRouter } from "next/navigation"
+
 function SchedulePage() {
+  const router = useRouter()
+
+  const {isInterviewer, isLoading} = useUserRole()
+
+  if (isLoading) return <LoaderUI />
   return (
-    <div>SchedulePage</div>
+    <div>
+      <InterviewScheduleUI />
+    </div>
   )
 }
 export default SchedulePage
